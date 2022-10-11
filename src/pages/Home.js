98d7962835d9a078b917c08c550267cc7de413c5
Dylan Wang <wyc6990906@hotmail.com>
2022-10-11 11:41:32 -0600

    get the booklist from db

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import {useState, useEffect} from 'react'
+import BookList from '../components/BookList'
+import BookForm from '../components/BookForm'
+//firebase 9
+import {db} from "../firebase/config"
+import {collection, getDocs} from "firebase/firestore"
+
+export default function Home() {
+  const [books, setBooks] = useState(null)
+  useEffect(() => {
+    const ref = collection(db, 'books')
+    getDocs(ref).then((snapshot) => {
+      let results = []
+      snapshot.docs.forEach(doc => {
+        results.push({
+          id: doc.id,
+          ...doc.data()
+        })
+      })
+      setBooks(results)
+    })
+  }, [])
+
+
+  return (
+    <div>
+      {books && <BookList books={books}/>}
+      <BookForm/>
+    </div>
+  );
+}
