c83c53fdb2f03b369d1407b64c8a7fe48c448f49
Dylan Wang <wyc6990906@hotmail.com>
2022-10-11 15:49:19 -0600

    real time data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,10 @@
 import {useState, useEffect} from 'react'
 import BookList from '../components/BookList'
 import BookForm from '../components/BookForm'
-//firebase 9
-import {db} from "../firebase/config"
-import {collection, getDocs} from "firebase/firestore"
+import {useCollection} from "../hooks/useCollection";
 
 export default function Home() {
-  const [books, setBooks] = useState(null)
-  useEffect(() => {
-    const ref = collection(db, 'books')
-    getDocs(ref).then((snapshot) => {
-      let results = []
-      snapshot.docs.forEach(doc => {
-        results.push({
-          id: doc.id,
-          ...doc.data()
-        })
-      })
-      setBooks(results)
-    })
-  }, [])
+  const {documents:books} = useCollection('books')
 
 
   return (
